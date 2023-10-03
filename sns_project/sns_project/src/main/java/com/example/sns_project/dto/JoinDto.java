5337dd0c68d0553ffb90e65c9f68f70b609dcc18
JEONGJIWON\wldnj <wldnjs9705@gmail.com>
2023-10-03 21:01:30 +0900

    Failed to deserialize java.time.LocalDateTime 오류를 수정하기 위해 joinDto 수정

--- sns_project/sns_project/src/main/java/com/example/sns_project/dto/JoinDto.java
+++ sns_project/sns_project/src/main/java/com/example/sns_project/dto/JoinDto.java
@@ -1,5 +1,6 @@
 package com.example.sns_project.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +14,6 @@ public class JoinDto {
     private String name;
     private String gender;
     private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDateTime birth;
 }
