02f3f6d7385fd0c944aa2f5d127945ec80ccc1fe
JEONGJIWON\wldnj <wldnjs9705@gmail.com>
2023-10-27 18:41:44 +0900

    친구 핸들러 로그분석을 위해 로그 추가

--- sns_project/sns_project/src/main/java/com/example/sns_project/controller/FriendController.java
+++ sns_project/sns_project/src/main/java/com/example/sns_project/controller/FriendController.java
@@ -21,35 +21,42 @@ public class FriendController {
     private final MemberService memberService;
     @GetMapping("/friend")
     public ResponseDto getFriendList(@AuthenticationPrincipal CustomDetails customDetails){
+        log.info("친구 리스트 핸들러");
         Member member = memberService.findByUsername(customDetails.getUsername());
         return friendService.findFriendList(member.getId());
     }
     @GetMapping("/friend/request")
     public ResponseDto getRequestFriendList(@AuthenticationPrincipal CustomDetails customDetails){
+        log.info("친구 요청 리스트 핸들러");
         Member member = memberService.findByUsername(customDetails.getUsername());
         return friendService.findWaitRequestFriendList(member.getId());
     }
     @GetMapping("/friend/requested")
     public ResponseDto getRequestedFriendList(@AuthenticationPrincipal CustomDetails customDetails){
+        log.info("친구 요청받은 리스트 핸들러");
         Member member = memberService.findByUsername(customDetails.getUsername());
         return friendService.findWaitRequestedFriendList(member.getId());
     }
     @PostMapping("/friend")
     public ResponseDto addFriend(@AuthenticationPrincipal CustomDetails customDetails, @RequestBody FriendDataDto friendDataDto){
+        log.info("친구 추가 핸들러");
         Member member = memberService.findByUsername(customDetails.getUsername());
         return friendService.save(new FriendDataDto(member.getId(), friendDataDto.getRequestedId(), null));
     }
     @GetMapping("/check/friend")
     public ResponseDto checkFriend(@AuthenticationPrincipal CustomDetails customDetails, @RequestBody FriendDataDto friendDataDto){
+        log.info("친구 체크 핸들러");
         Member member = memberService.findByUsername(customDetails.getUsername());
         return friendService.checkFriendRequest(new FriendDataDto(member.getId(), friendDataDto.getRequestedId(), null));
     }
     @PostMapping("/friend/accept")
     public ResponseDto acceptFriend(@RequestBody FriendDto friendDto){
+        log.info("친구 수락 핸들러");
         return friendService.acceptFriendRequest(friendDto.getId());
     }
     @DeleteMapping("/friend")
     public ResponseDto deleteFriend(@RequestBody FriendDto friendDto){
+        log.info("친구 삭제 핸들러");
         return friendService.deleteFriend(friendDto);
     }
 }
