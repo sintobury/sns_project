d1ec8c9b66782e3f217220c047cad517e05897c2
JEONGJIWON\wldnj <wldnjs9705@gmail.com>
2023-11-11 17:17:56 +0900

    BoardDto 수정

--- sns_project/sns_project/src/main/java/com/example/sns_project/dto/BoardDto.java
+++ sns_project/sns_project/src/main/java/com/example/sns_project/dto/BoardDto.java
@@ -1,16 +1,15 @@
 package com.example.sns_project.dto;
 
 import com.example.sns_project.entity.Member;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardDto {
